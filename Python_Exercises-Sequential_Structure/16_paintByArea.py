befa6de27c7598dc2556b9c3e6784da987cffb1f
L. A. Souza <lasouzadesigner@gmail.com>
2020-03-02 16:04:14 -0300

    update commit

--- Python_Exercises-Sequential_Structure/16_paintByArea.py
+++ Python_Exercises-Sequential_Structure/16_paintByArea.py
@@ -1,9 +1,3 @@
-# Faça um programa para uma loja de tintas. O programa deverá pedir o tamanho
-# em metros quadrados da área a ser pintada. Considere que a cobertura da tinta
-# é de 1 litro para cada 3 metros quadrados e que a tinta é vendida em latas de
-# 18 litros, que custam R$ 80,00. Informe ao usuário a quantidades de latas de
-# tinta a serem compradas e o preço total.
-
 # Make a program for a paint store. The program should ask for the size
 # in square meters of the area to be painted. Consider that the ink coverage
 # is 1 liter for every 3 square meters and that the paint is sold in cans of
@@ -11,3 +5,12 @@
 # ink to be purchased and the total price.
 
 
+meters = float(input('Insert the area in square meters: '))
+
+cans = (meters / 3) / 18
+
+if cans != int(cans): cans = round(cans + 0.5)
+
+cost = cans * 80
+
+print(f'\nYou need {int(cans)} cans and will pay {cost:.2f}R$ for it')
--- Python_Exercises-Sequential_Structure/18_downloadTime.py
+++ Python_Exercises-Sequential_Structure/18_downloadTime.py
@@ -1,9 +1,13 @@
-# Faça um programa que peça o tamanho de um arquivo para download (em MB)
-# e a velocidade de um link de Internet (em Mbps), calcule e informe o tempo
-# aproximado de download do arquivo usando este link (em minutos).
-
 # Make a program that asks for the size of a file to download (in MB)
 # and the speed of an Internet link (in Mbps), calculate and report the time
 # approximate file download using this link (in minutes).
 
 
+size = float(input('Insert the file size: '))
+speed = float(input('Insert the speed of an internet link in Mbps: '))
+
+if size != int(size): size = round(size + 0.5)
+
+time = (size / speed) / 60
+
+print(f'\napproximate time to download this file: {time:.2f} minutes')
